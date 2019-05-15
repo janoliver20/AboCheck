0463eecd91735279f77badca5f49a2d2ea23c554
Benedikt Langer <S1810237024@students.fh-hagenberg.at>
2019-05-15 14:50:12 +0200

    new Abo first edit

---
+++ AboCheck/NewAboViewController.swift
@@ -0,0 +1,25 @@
+//
+//  NewAboViewController.swift
+//  AboCheck
+//
+//  Created by Benedikt Langer on 15.05.19.
+//  Copyright © 2019 Cortiel_Langer_Wagner. All rights reserved.
+//
+
+import UIKit
+
+class NewAboViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        
+    }
+    
+
+    @IBAction func addAbo(_ sender: UIBarButtonItem) {
+        
+        dismiss(animated: true)
+    }
+    
+}
