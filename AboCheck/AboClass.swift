6493125d3886e84aaeed9666e140a2d19644e129
Jan Cortiel <jan.cortiel@gmail.com>
2019-05-15 15:54:44 +0200

    Data loading and saving with a new class.

---
+++ AboCheck/AboClass.swift
@@ -0,0 +1,67 @@
+//
+//  AboClass.swift
+//  AboCheck
+//
+//  Created by Jan Cortiel on 15.05.19.
+//  Copyright © 2019 Cortiel_Langer_Wagner. All rights reserved.
+//
+
+import UIKit
+import CoreData
+
+class AboClass {
+// A static variable to access the data from everywhere in this project.
+    static let allAbos = AboClass()
+    
+// Array for all data, which get initialized directly at startup
+    var abos: [Abo] = [Abo]()
+    
+    private init(){}
+    
+    func saveAbo(title: String, note: String = "", website: URL? = nil, creationDate: Date = Date(), endDate: Date? = nil, costsMonthly: Double = 0.0, catagory: String = "" ) {
+        guard let appDelegate = UIApplication.shared.delegate as? AppDelegate else {
+            return
+        }
+//
+        let managedContext = appDelegate.persistentContainer.viewContext
+//
+        let entity = NSEntityDescription.entity(forEntityName: "Abo", in: managedContext)!
+//
+        let abo = NSManagedObject(entity: entity, insertInto: managedContext)
+//  set Values to the keys.
+        abo.setValue(title, forKey: "title")
+        abo.setValue(note, forKey: "note")
+        abo.setValue(website, forKey: "website")
+        abo.setValue(creationDate, forKey: "creationDate")
+        abo.setValue(costsMonthly, forKey: "costsMonthly")
+        abo.setValue(endDate, forKey: "endDate")
+        abo.setValue(catagory, forKey: "catagory")
+        
+        do {
+            try managedContext.save()
+        } catch let error as NSError {
+            print("Error: \(error)")
+        }
+        
+    }
+    
+    func loadAbo() {
+        guard let appDelegate = UIApplication.shared.delegate as? AppDelegate else {
+            return
+        }
+        
+        let managedContext = appDelegate.persistentContainer.viewContext
+        let fetchRequest = NSFetchRequest<NSManagedObject>(entityName: "Abo")
+        
+        do{
+            abos = try managedContext.fetch(fetchRequest) as! [Abo]
+        } catch let error as NSError {
+            print("Error: \(error)")
+        }
+    }
+    
+    
+    
+    
+    
+}
--- AboCheck/ListViewController.swift
+++ AboCheck/ListViewController.swift
@@ -9,22 +9,20 @@
 import UIKit
 
 class ListViewController: UIViewController {
+    
+    let allAbos = AboClass.allAbos
 
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+        
+        
+    }
+    override func viewWillAppear(_ animated: Bool) {
+        allAbos.loadAbo()
     }
     
+    
 
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
-    }
-    */
+    
 
 }
